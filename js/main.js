27ec84d0cbe3efa0d605219301666325f6851179
Okarinnasai <kevin.19.timido@gmail.com>
2022-07-10 11:15:25 -0500

    10/07/22 --01

--- js/main.js
+++ js/main.js
@@ -8,3 +8,46 @@ window.onscroll = () => {
 	}
 }
 
+function redirect()
+{
+   
+    window.location.href="productos.html";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const elementoCarousel = document.querySelectorAll ('.carousel');
+	M.Carousel.init(elementoCarousel, {
+		duration: 150,
+		dist: -80,
+		shift: 5,
+		padding: 5,
+		numVisible: 3,
+		indicators: true,
+		noWrap: false,
+	});
+	
+	//version 2.0 de autoplay :v
+	var autoplay = true;
+	setInterval(()=>{
+		if(autoplay == true){
+			M.Carousel.getInstance(elementoCarousel[0]).next();
+		}
+		
+	 },2000);
+
+	 let id_Carousel = document.getElementById('foto-p');
+	 
+	 id_Carousel.addEventListener("mouseover", function(event){
+		autoplay=false;
+	 });
+
+	 id_Carousel.addEventListener("mouseout", function(event){
+		autoplay=true;
+	 })
+
+
+});
+	
+
+
+	
